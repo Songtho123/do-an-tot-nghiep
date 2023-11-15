b25de596d204a2b5bb63718701e1eb5e5c8858b3
Nguyen Huy <huy246302@gmail.com>
2023-11-15 19:45:42 +0700

    center-div

--- single_page.php
+++ single_page.php
@@ -208,7 +208,7 @@
 
 </main>
 
-<footer class="blog-footer">
+<footer class="row blog-footer">
   <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
   <p>
     <a href="#">Back to top</a>
